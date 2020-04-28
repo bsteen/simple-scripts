de90330f9a80d6e0f5b0080b8d16c578d3d84c00
bsteen <bsteen@github.com>
2020-04-28 13:01:36 -0400

    Use dist-upgrade now.

--- upgrade.sh
+++ upgrade.sh
@@ -2,6 +2,6 @@
 # Benjamin Steenkamer, 2020.
 # Fetch and update packages; then remove old packages.
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get dist-upgrade
 sudo apt-get autoclean
 sudo apt-get autoremove
