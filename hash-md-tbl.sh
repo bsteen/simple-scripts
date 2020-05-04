6f1b6322118c68606e46f596cb242e963b511b8f
bsteen <bsteen@github.com>
2020-05-04 00:10:26 -0400

    Added CRC-32 to table.

--- hash-md-tbl.sh
+++ hash-md-tbl.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 # Benjamin Steenkamer, 2020.
-# Calculates the hashes of all the files in a folder and then generates a
-# Markdown-style table of the files and hashes. It generates CRC32 and SHA-256
-# hashes and outputs them into `output.md`.
+# Calculates the checksum/hashes of all the files in a folder and then generates a
+# Markdown-style table of the results. The results are outputed into `output.md`.
 #
 # TODO:
 # Specify the folder to hash
-# Add CRC32 and file size (bytes)
 # Specify output file name: -o "file_name"
 # Specify file filter (only hash these files): -f "*.iso|*.bin"
 # Hash files in sub-folders
@@ -17,13 +15,14 @@ files=$(ls -1 | sort -V)
 printf "Creating hash table...\n"
 
 > $output_name
-printf "| File| Size (B) | SHA-256 |\n" >> $output_name
-printf "|---|---|---|\n" >> $output_name
+printf "| File| Size (B) | CRC-32 | SHA-256 |\n" >> $output_name
+printf "|---|---|---|---|\n" >> $output_name
 
 while read file; do
     size=$(du -b "$file" | awk '{print $1}')
     printf "Hashing $file...\n"
-    hash=$(sha256sum "$file" | awk '{print $1}')
-    printf "| $file | $size | \`$hash\` |\n" >> $output_name
+    crc=$(crc32 "$file")
+    sha=$(sha256sum "$file" | awk '{print $1}')
+    printf "| $file | $size | \`$crc\`  | \`$sha\` |\n" >> $output_name
 done <<< $files
 printf "Done.\n"
