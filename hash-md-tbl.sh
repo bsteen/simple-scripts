058b54817e00c945d93e473af78bcf8bfb3dd14b
bsteen <bsteen@github.com>
2020-05-03 23:37:43 -0400

    Added file size to table.

--- hash-md-tbl.sh
+++ hash-md-tbl.sh
@@ -17,13 +17,13 @@ files=$(ls -1 | sort -V)
 printf "Creating hash table...\n"
 
 > $output_name
-printf "| File | SHA-256 |\n" >> $output_name
-printf "|---|---|\n" >> $output_name
+printf "| File| Size (B) | SHA-256 |\n" >> $output_name
+printf "|---|---|---|\n" >> $output_name
 
 while read file; do
+    size=$(du -b "$file" | awk '{print $1}')
     printf "Hashing $file...\n"
-    hash=$(sha256sum "$file")
-    hash=$(echo $hash | awk '{print $1}')
-    printf "| $file | \`$hash\` |\n" >> $output_name
+    hash=$(sha256sum "$file" | awk '{print $1}')
+    printf "| $file | $size | \`$hash\` |\n" >> $output_name
 done <<< $files
 printf "Done.\n"
