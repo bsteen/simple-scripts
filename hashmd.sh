9fc8681ea760d5475828d5674e5b49d567fa4c14
bsteen <bsteen@github.com>
2020-05-09 16:00:58 -0400

    Can now specify directory to hash. * Updated arg parse to handle specifying directory. * Use `find` in both base and recursive search. * Cut off leading "./" in file names.

--- hashmd.sh
+++ hashmd.sh
@@ -1,42 +1,46 @@
 #!/bin/bash
 # (C) 2020 Benjamin Steenkamer.
-# Calculates the checksum/hashes of all the files in a folder and then generates a
-# Markdown-style table of the results. The results are outputted into `output.md`.
+# Calculates the checksum/hashes of all the files in a folder and then
+# generates a # Markdown-style table of the results. The results are outputted
+# into `output.md`.
 # Usage:
-# Create table of files in current directory:
-#   ./hashmd.sh
-# Create table of files in current directory and all sub directories:
-#   ./hashmd.sh -r
+# Create table from files in given directory (no directory means current one):
+#   `./hashmd.sh path/to/dir` or `./hashmd.sh`
+# Create table of files in given directory and all sub directories:
+#   `./hashmd.sh -r path/to/dir` or `./hashmd.sh -r`
 # TODO:
-# Specify the folder to hash
 # Specify output file name: -o "file_name"
 # Specify file filter (only hash these files): -f "*.iso|*.bin"
 
 output_name="output.md"
+dir="."
+recur=false
 
-# Specify base folder
-# ./script -d "a\b\c"
-# dir="."
-# dir=$1
-# ls "$dir"
+# Parse arguments
+for arg in "$@"; do
+	if [[ -d "$arg" ]]; then
+		dir=$arg
+	elif [ "$arg" = "-r" ]; then
+        recur=true
+	fi
+done
 
-if [ "$1" == "-r" ]; then
-    # Find all files and directories recursively and cut of leading "./"
-    files=$(find . | sort -f | cut -c 3-)
+# Create file list
+if [ $recur = true ]; then
+    # Find all files and directories recursively, sort case-insensitive
+    files=$(find "$dir" | sort -f)
 else
-    # List files and directories only in base directory
-    files=$(ls -1 | sort -f)
+    # List files and directories only in base directory, sort case-insensitive
+    files=$(find "$dir" -maxdepth 1 | sort -f)
 fi
 
-> $output_name      # Create/clear output file
+> $output_name      # Create or truncate output file
 printf "| File| Size (B) | CRC-32 | SHA-256 |\n" >> $output_name
 printf "|---|---|---|---|\n" >> $output_name
 
 while read file; do
-    # If it is actually a file (and not a directory), hash it
-    if [[ -f $file ]]; then
-        # Don't hash this script file
-        if [ $file = "hashmd.sh" ]; then
+    if [[ -f "$file" ]]; then         # If it is actually a file, hash it
+        if [ "$file" = "hashmd.sh" ]; then    # Don't hash this script file
             continue
         fi
 
@@ -44,6 +48,11 @@ while read file; do
         size=$(du -b "$file" | awk '{print $1}')
         crc=$(crc32 "$file")
         sha=$(sha256sum "$file" | awk '{print $1}')
+
+        if [[ "$file" = "./"* ]]; then
+            file=$(echo "$file" | cut -c 3-)    # Cut off the leading "./"
+        fi
+
         printf "| $file | $size | \`$crc\`  | \`$sha\` |\n" >> $output_name
     fi
 done <<< $files
