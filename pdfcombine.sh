6559634945de73c83e9a5d300a95ae1949c03e21
bsteen <bsteen@github.com>
2020-07-26 01:36:51 -0400

    Added TODO.

--- pdfcombine.sh
+++ pdfcombine.sh
@@ -3,5 +3,6 @@
 # Combine multiple .pdf files into a single .pdf.
 # Usage: ./pdfcombine.sh input1.pdf input2.pdf...
 # Remove spaces from the input file names before combining.
+# TODO: Get files names with spaces to work.
 
 pdftk $* cat output output.pdf
