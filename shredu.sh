8b946d3ccb79a9a334d04fdc714f15dfff33b974
Benjamin Steenkamer <13274734+bsteen@users.noreply.github.com>
2021-04-12 17:58:49 -0400

    Added copyright notice.

--- shredu.sh
+++ shredu.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# (C) 2021 Benjamin Steenkamer
+# 
 # Overwrites the specified file(s) with 3 passes of random data, 
 # a final 4th pass of all zeros, and then removes the file(s).
 # Output is set to verbose to show all steps.
