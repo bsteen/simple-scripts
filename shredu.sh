4857c40823e11bb8695b79c966b89de29e694966
bsteen <bsteen@github.com>
2022-03-31 21:08:14 -0400

    Only 2 random passes now, 3 is excessive

--- shredu.sh
+++ shredu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # (C) 2021 Benjamin Steenkamer
-# 
-# Overwrites the specified file(s) with 3 passes of random data, 
-# a final 4th pass of all zeros, and then removes the file(s).
+#
+# Overwrites the specified file(s) with 2 passes of random data,
+# a final pass of all zeros, and then removes the file(s).
 # Output is set to verbose to show all steps.
-shred -vzun 3 "$@"
+shred -vzun 2 "$@"
