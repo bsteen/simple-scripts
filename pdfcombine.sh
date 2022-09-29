25780fbf8f28430a5e29fed700172cce0a1874df
bsteen <13274734+bsteen@users.noreply.github.com>
2022-09-29 01:43:22 -0400

    Removed TODO for fixing spaces

--- pdfcombine.sh
+++ pdfcombine.sh
@@ -2,7 +2,5 @@
 # (C) 2020 Benjamin Steenkamer.
 # Combine multiple .pdf files into a single .pdf.
 # Usage: ./pdfcombine.sh input1.pdf input2.pdf...
-# Remove spaces from the input file names before combining.
-# TODO: Get files names with spaces to work.
 
 pdftk "$@" cat output output.pdf verbose    # "$@" expands to separate words: "$1" "$2" ...
