a6df7b592e6de0e9a304dc6177e08ed9f1d03d52
bsteen <13274734+bsteen@users.noreply.github.com>
2022-09-29 01:45:55 -0400

    Minor changes in naming, printout

--- pdf2png2pdf.sh
+++ pdf2png2pdf.sh
@@ -8,10 +8,10 @@ mkdir -p p2p2p-temp
 
 # density = PNG dots-per-inch (DPI)
 echo "Converting PDF to PNGs..."
-convert -density 300 "$1" p2p2p-temp/temp.png
+convert -density 300 "$1" p2p2p-temp/page.png
 
 echo "Converting PNGs to PDF..."
 # Get PNG names by numeric order
 convert $(ls -1 p2p2p-temp/*.png | sort -V) $1-converted.pdf
 rm -rf p2p2p-temp
-echo "Done."
+echo "Done"
