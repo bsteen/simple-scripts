9aa79fa29af48b00f61795930a9393d5452d9124
bsteen <bsteen@github.com>
2023-06-19 01:45:47 -0400

    Print useage message/help if 2 args are not given - Makes it quicker to see how script works

--- sha256_verify.sh
+++ sha256_verify.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-# (C) 2022 Benjamin Steenkamer.
+# (C) 2022 - 2023 Benjamin Steenkamer.
 # Check if the calculated SHA256 sum of file equals the provided SHA256 sum string.
 # This saves you the time of making a formatted file with the sum and filename.
-# Usage: ./sha256_verify myfiletocheck.exe d14a028c2a3a2bc9476102bb288234c415a2b01f828ea62ac5b3e42f
-
-echo "$2 $1" | sha256sum --check
+if [ -z $1 ] || [ -z $2 ]; then
+	echo "Usage: $0 <file> <SHA-256-str>"
+else
+	echo "$2 $1" | sha256sum --check
+fi
