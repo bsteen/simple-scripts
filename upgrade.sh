a80e3f793882cc6242490616e238325212e91946
bsteen <bsteen@github.com>
2023-10-29 21:19:05 -0400

    Use `autopurge` instead of `autoremove`

--- upgrade.sh
+++ upgrade.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# (C) 2020 Benjamin Steenkamer.
+# (C) 2020 - 2023 Benjamin Steenkamer.
 # Fetch and update packages; then remove old packages.
 sudo apt-get update
 sudo apt-get dist-upgrade
 sudo apt-get autoclean
-sudo apt-get autoremove
+sudo apt-get autopurge
