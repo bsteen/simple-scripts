7ac66b1b185e6f129748ec6ead10cd7381a43eab
bsteen <13274734+bsteen@users.noreply.github.com>
2023-12-14 15:26:49 -0500

    Fix empty string check not working when string contains unquoted white spaces

--- sha256-check.sh
+++ sha256-check.sh
@@ -2,7 +2,7 @@
 # (C) 2022 - 2023 Benjamin Steenkamer.
 # Check if the calculated SHA256 sum of file equals the provided SHA256 sum string.
 # This saves you the time of making a formatted file with the sum and filename.
-if [ -z $1 ] || [ -z $2 ]; then
+if [[ -z $1 ]] || [[ -z $2 ]]; then
 	echo "Usage: $0 <file> <SHA-256-str>"
 else
 	echo "$2 $1" | sha256sum --check
