332bab118df477d9c4bf3950b673410dec4d9df0
bsteen <13274734+bsteen@users.noreply.github.com>
2023-12-14 15:25:31 -0500

    Update comments

--- pdf2png2pdf.sh
+++ pdf2png2pdf.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# (C) 2020 Benjamin Steenkamer.
+# (C) 2020 - 2023 Benjamin Steenkamer
 # Convert .pdf to .png and then back to .pdf.
-# Try to  maintain good quaility in the process (increase density if bad).
-# Useful for deactivating links in the .pdf.
+# Try to  maintain good quality in the process (increase the density if results are not good enough).
+# Useful for removing hyperlinks and searchable text in PDFs.
 
 mkdir -p p2p2p-temp
 
