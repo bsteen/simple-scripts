577cc8a4fffdadd7df6b573b2d5e2b6e381f9551
Benjamin Steenkamer <13274734+bsteen@users.noreply.github.com>
2024-08-24 22:24:45 -0400

    Return alpha-numeric with `-a`

--- rand-str.sh
+++ rand-str.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-# (C) 2020 - 2023 Benjamin Steenkamer
-# Get 128 btyes of random data from /dev/urandom and return visible ASCII characters only.
-# Passing `-a` makes only letters (upper and lower) be retuned.
+# (C) 2020 - 2024 Benjamin Steenkamer
+# Get 128 btyes of random data from /dev/urandom and return visible ASCII characters only
+# Passing `-a` makes only alpha-numeric characters be retuned
+
 if [ "$1" = "-a" ]; then
 	# Delete the complement of set 1: only have letters
-	head -c 128 /dev/urandom | tr -dc [:alpha:]
+	head -c 128 /dev/urandom | tr -dc [:alnum:]
 else
 	# Delete the complement of set 1: only have visible ASCII characters
 	head -c 128 /dev/urandom | tr -dc [:graph:]
