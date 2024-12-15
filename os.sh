5786f9e8c6b41b249a746b6bdf1568e56633f0d7
bsteen <13274734+bsteen@users.noreply.github.com>
2024-12-15 12:44:26 -0500

    Script to print basic distro details

---
+++ os.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Prints basic details of Debian distro:
+# Debian major.minor, codename, and architecture
+# Kernel version and release date
+# Current up time and start time
+
+os_release=$(cat /etc/os-release)
+name=$(awk -F= '/^NAME=/ {print $2}' <<< $os_release | tr -d '"')
+codename=$(awk -F= '/^VERSION_CODENAME=/ {print $2}' <<< $os_release)
+
+version=$(cat /etc/debian_version)
+
+echo "$name $version ($codename)" $(uname -m)
+uname -srv
+echo $(uptime -p) \($(uptime -s)\)
