8cfd97ce1e0491a59e7277f18ba3c5f1322a2dba
Benjamin Steenkamer <13274734+bsteen@users.noreply.github.com>
2025-11-08 16:17:08 -0500

    Rework for debian based distro

--- os.sh
+++ os.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-# (C) 2024 Benjamin Steenkamer
-# Prints basic details of Debian distro:
-# Debian major.minor, codename, and architecture
+# (C) 2024 - 2025 Benjamin Steenkamer
+# Prints basic details of Debian-based distro:
+# Distro name, version, code name, and architecture
+# Debian version and code name
 # Kernel version and release date
 # Current up time and start time
 
 os_release=$(cat /etc/os-release)
-name=$(awk -F= '/^NAME=/ {print $2}' <<< $os_release | tr -d '"')
-codename=$(awk -F= '/^VERSION_CODENAME=/ {print $2}' <<< $os_release)
+dist_name=$(awk -F= '/^PRETTY_NAME=/ {print $2}' <<< $os_release | tr -d '"')
+deb_name=$(awk -F'=' '/DEBIAN_CODENAME/ {print $2}' <<< $os_release)
+deb_version=$(awk -F'=' '/DEBIAN_VERSION_FULL/ {print $2}' <<< $os_release)
 
-version=$(cat /etc/debian_version)
 
-echo "$name $version ($codename)" $(uname -m)
+echo "$dist_name" $(uname -m)
+echo "Debian $deb_version ($deb_name)"
 uname -srv
 echo $(uptime -p) \($(uptime -s)\)
