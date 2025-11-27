5db6e146d009eca3fe1fc57808f5277d1846d220
Benjamin Steenkamer <13274734+bsteen@users.noreply.github.com>
2025-11-27 14:28:35 -0500

    Use `tmp/` dir for files

--- pdf2png2pdf.sh
+++ pdf2png2pdf.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-# (C) 2020 - 2023 Benjamin Steenkamer
+# (C) 2020 - 2023, 2025 Benjamin Steenkamer
 # Convert .pdf to .png and then back to .pdf.
 # Try to  maintain good quality in the process (increase the density if results are not good enough).
 # Useful for removing hyperlinks and searchable text in PDFs.
 
-mkdir -p p2p2p-temp
+TEMP_DIR=$(mktemp -d)
 
 # density = PNG dots-per-inch (DPI)
 echo "Converting PDF to PNGs..."
-convert -density 300 "$1" p2p2p-temp/page.png
+convert -density 300 "$1" ${TEMP_DIR}/page.png
 
 echo "Converting PNGs to PDF..."
 # Get PNG names by numeric order
-convert $(ls -1 p2p2p-temp/*.png | sort -V) "${1%.pdf}-converted.pdf"
-rm -r p2p2p-temp
+convert $(ls -1 ${TEMP_DIR}/*.png | sort -V) "${1%.pdf}-converted.pdf"
+rm -rf ${TEMP_DIR}
 echo "Done"
